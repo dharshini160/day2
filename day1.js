201e55449f6225c8ae458b9d4ce8e6478ac96f4c
dharshini160 <dharshinikrishnan77@gmail.com>
2025-07-12 10:45:15 +0530

    day1.js

---
+++ day1.js
@@ -0,0 +1,21 @@
+//task 1
+var number=16;
+var name='dharsh';
+var iscloudy=true;
+console.log(number+""+name+""+iscloudy);
+document.write(number);
+document.write(name);
+document.write(iscloudy);
+
+//task 2
+console.log(typeof(number));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+
+//task 3
+var n1=parseInt(prompt("enter the first value"));
+var n2=parseInt(prompt("enter the second value"));
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
