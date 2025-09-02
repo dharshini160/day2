ed5eab6a9401b4db1543246f5bce8f88b4caa7c6
dharshini160 <dharshinikrishnan77@gmail.com>
2025-09-02 14:12:54 +0530

    day8.js

---
+++ day8.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        let currenttime=new Date();
+console.log(currenttime);
+let local=currenttime.toString();
+console.log(local);
+let hours=currenttime.getHours();
+console.log(hours);
+let minits=currenttime.getMinutes();
+console.log(minits);
+let day=currenttime.getDay();
+console.log(day);
+class Exam{
+      constructor(personname){
+        this.personname=personname;
+        console.log(personname);
+      }
+}
+let result=new Exam("dharsh")
+class Bank{
+    constructor(initialamount=0 ){
+        this._balance=initialamount;
+    }
+    get balance(){
+        return this._balance;
+    }
+    set balance(amountdeposit){
+        if(amountdeposit>0){
+            this.balance += amountdeposit;
+            console.log(_balance);
+        }
+    }
+}
+let add=new Bank(10);
+console.log(add);
+add._balance=300;
+console.log(add);
+console.log(add.balance);
+
+
+    </script>
+    
+</body>
+</html>
